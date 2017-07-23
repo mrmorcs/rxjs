6c656ac5456b0c154c3d362186ec40d82edfa4ff
mrmorcs <morcs@morcs.com>
2017-07-23 16:52:21 +0100

    Error handling from fetch.

--- main.ts
+++ main.ts
@@ -1,25 +1,10 @@
 import { Observable } from "rxjs";
+import { load, loadWithFetch } from "./loader";
 
 let output = document.getElementById("output");
 let button = document.getElementById("button");
 
 let click = Observable.fromEvent(button, "click");
-
-function load(url: string) {
-    return Observable.create(observer => {
-
-        let xhr = new XMLHttpRequest();
-
-        xhr.addEventListener("load", () => {
-            let data = JSON.parse(xhr.responseText);
-            observer.next(data);
-            observer.complete();
-        });
-
-        xhr.open("GET", url);
-        xhr.send();
-    });
-}
 
 function renderMovies(movies) {
     movies.forEach(m => {
@@ -29,7 +14,13 @@ function renderMovies(movies) {
     });
 }
 
-click.flatMap(e => load("movies.json"))
+loadWithFetch("moviess.json").subscribe(
+    renderMovies,
+    error => console.log(`error: ${error}`),
+        () => console.log("complete")
+);
+
+click.flatMap(e => loadWithFetch("movies.json"))
     .subscribe(
         renderMovies,
         error => console.log(`error: ${error}`),
