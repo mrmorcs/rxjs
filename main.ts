3320dff7c445d0e7f1b52fe1cb9efec7d7b8d83d
mrmorcs <morcs@morcs.com>
2017-07-23 12:31:43 +0100

    Moving curcle thing.

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-alert("ok!");
+import { Observable } from "rxjs";
+
+let circle = document.getElementById("circle");
+let source = Observable.fromEvent(document, "mousemove")
+                       .map((e: MouseEvent) => {
+                           return {
+                                x: e.clientX,
+                                y: e.clientY
+                           }
+                       })
+                       .filter(e => e.x < 500)
+                       .delay(300);
+ 
+
+let go = () => { 
+    source.subscribe(
+        onNext,
+        error => console.log(`error: ${error}`),
+        () => console.log("complete")
+    )
+};
+
+function onNext(value) {
+    circle.style.left = value.x;
+    circle.style.top = value.y;
+}
+
+go();
+setTimeout(go, 5000);
